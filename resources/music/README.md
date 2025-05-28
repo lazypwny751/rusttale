9368533295068dca84f108d85eb90d371140330e
lazypwny751 <testfordnshijacking@gmail.com>
2025-05-28 20:52:22 +0300

    added, main menu music (wide putin walk 'song for denise')

---
+++ resources/music/README.md
@@ -0,0 +1,2 @@
+# Note.
+That compoment's copyrighted, so this situation for temporary. After creat the musics from midi trackers, don't forget to remove them.
--- src/game/state.rs
+++ src/game/state.rs
@@ -1,12 +1,43 @@
-use tetra::{graphics::{ self, Color, text::Text, text::Font }, State, Context };
+// imports.
+use tetra::audio::{self, Sound, SoundInstance};
+use tetra::{graphics::{ 
+	self, Color, text::Text, text::Font }, 
+	State, Context 
+};
 use crate::game::defaults::TEXT_OFFSET;
 use crate::TTableOne;
 
+// Game State.
 pub struct GameState {
 	text: Text,
+	channel: SoundInstance
 }
 
+// And the gamestate.
+impl GameState {
+	pub fn new(ctx: &mut Context, language: &str) -> tetra::Result<Self> {
+		audio::set_master_volume(ctx, 0.4);
+
+        let sound = Sound::new("resources/music/songfordenise.mp3")?;
+        let channel = sound.spawn(ctx)?;
+
+		#[allow(clippy::useless_format)]
+		let text = Text::new(format!("{}", TTableOne::localize("hello world", language)),
+			Font::vector(ctx, "resources/font/SourceCodePro-Regular.ttf", 16.0)?,
+		);
+
+		Ok(GameState { text, channel })
+	}
+}
+
+// State implementation.
 impl State for GameState {
+	fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+		self.channel.play();
+
+		Ok(())
+	}
+
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         // Cornflower blue, as is tradition
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
@@ -15,15 +46,4 @@ impl State for GameState {
 
         Ok(())
     }
-}
-
-impl GameState {
-	pub fn new(ctx: &mut Context, language: &str) -> tetra::Result<GameState> {
-		#[allow(clippy::useless_format)]
-		let text = Text::new(format!("{}", TTableOne::localize("hello world", language)),
-			Font::vector(ctx, "resources/font/SourceCodePro-Regular.ttf", 16.0)?,
-		);
-
-		Ok(GameState { text })
-	}
 }
