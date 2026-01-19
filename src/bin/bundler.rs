d7fd1f76abb725805b54f56dc6c861a1f48bcf5f
lazypwny751 <testfordnshijacking@gmail.com>
2026-01-20 01:02:27 +0300

    add bundler, remove bin/ from gitignore

---
+++ src/bin/bundler.rs
@@ -0,0 +1,15 @@
+/*
+	> Rusttale, 2026 - bundler 
+	Binary asset bundler for the Rusttale project.
+	This helper tool builds game resources into a single resource pack
+	that can be loaded by the Rusttale engine. 
+
+	Usage; 
+
+	~$ bundler --path <path>:
+		the <path> must be include "resourcetale.toml" and all those referances
+		based on <path 
+*/
+fn main() {
+	println!("rusttale resource bundler");	
+}
